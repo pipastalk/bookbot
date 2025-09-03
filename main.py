90c86823c14532582bf60f24de151cd0e3f48a50
pipastalk <pipalabs@luckyfixes.co.uk>
2025-09-03 22:40:31 +0100

    built sort dictionary and adjusted main to support cli parameters

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from stats import get_num_words
+import sys
+from stats import get_num_words, character_count, sort_dictionary
 def get_book_text(inputPath):
     with open(inputPath) as f:
         return f.read()
-def main(): 
-    book_text = get_book_text("books/frankenstein.txt")
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>") 
+        sys.exit(1)
+    else:    
+        path = sys.argv[1]
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {path}...\n----------- Word Count ----------" )
+    book_text = get_book_text(path)
+    book_char_count = character_count(book_text)
     #print(book_text)
-    print(f"{get_num_words(book_text)} words found in the document")
+    print(f"Found {get_num_words(book_text)} total words")
+    print(f"{character_count(book_text)}")
+    print("(--------- Character Count -------)")
+    #print(f"{sort_dictionary(book_char_count)}")
+    sorted_characters = sort_dictionary(book_char_count)
+    for k,v in sorted_characters.items():
+        if k.isalpha():
+            print(f"{k}: {v}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+def character_count(text):
+    character_count = {}
+    unique_characters = set(text.lower())
+    for character in unique_characters:
+        character_count[character] = text.lower().count(character)
+    return character_count
+
+
+def sort_dictionary(d):
+    def sort_on(items):
+        type(items)
+        return items[1]
+    sorted_dict = dict(sorted(d.items(), key=sort_on, reverse=True))
+    return sorted_dict
